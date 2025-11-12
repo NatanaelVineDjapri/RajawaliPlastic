a41371d87029afd9de0fa0d2cab9fbdfa8c3e426
edbert19 <ed79bert@gmail.com>
2025-11-12 23:06:10 +0700

    connecting backend to gallery section

--- FrontEnd/app/dashboard/layout.tsx
+++ FrontEnd/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Sidenavbar from '../components/admincomponents/sidenavbar';
-import TopNavbar from '../components/admincomponents/topnavbar';
+import Sidenavbar from '../components/admincomponents/SideNavBar';
+import TopNavbar from '../components/admincomponents/TopNavBar';
 import CreateButton from '@/app/components/admincomponents/CreateButton';
 import React from 'react';
 import { AuthProvider } from '@/app/contexts/AuthContext';
